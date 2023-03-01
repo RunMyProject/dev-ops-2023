b41edbebaed19fb263739b625f999e3a67a73603
RunMyProject <esabatini74@gmail.com>
2023-03-01 20:17:25 +0100

    added parser Tika

--- checkmime/src/main/java/myspring/checkmime/controller/ValidationController.java
+++ checkmime/src/main/java/myspring/checkmime/controller/ValidationController.java
@@ -22,6 +22,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.tika.*;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.config.TikaConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,7 +55,7 @@ public class ValidationController {
 	public ResponseEntity<List<LayoutResponse>> validationAll() {
 
 		List<LayoutResponse> layoutResponses = new ArrayList<>();
-		List<String> files = new ArrayList<>();
+		List<Path> files = new ArrayList<>();
 
 		AtomicInteger itemAtomicInteger = new AtomicInteger();
 		AtomicInteger pageAtomicInteger = new AtomicInteger();
@@ -55,52 +65,82 @@ public class ValidationController {
 
 		List<Checkmime> checkmimeList = checkmimeRepository.findByEnabled(true);
 
+		// Initialize p7m format
+		//
 		boolean enabled_p7m = false;
+		String description_p7m = null;
 
 		for(Checkmime checkmime : checkmimeList) {
 			if(checkmime.getFormat().equals(MIME_FORMAT)) {
 				enabled_p7m = checkmime.isEnabled();
+				description_p7m = checkmime.getDescription();
 				break;
 			}
 		}
 
 		for(Path path : fileSystemStorage.listFiles()) {
 
-			String fileName = path.getFileName().toString();
 			int nFile = itemAtomicInteger.incrementAndGet();
 
 			if(nFile==1) files.clear();
-			files.add(fileName);
+			files.add(path);
 
 			if(nFile==validationService.getPageNumber() || (nFile==totLast && pageAtomicInteger.get()==totPages)) {
 
 				List<ValidationFormat> validationList = new ArrayList<>();
 
-				for(String file : files) {
+				for(Path filePath : files) {
 
-					boolean validated = false;
+					String fileName = filePath.getFileName().toString();
 
 					String extension = "";
-					int i = file.lastIndexOf(MIME_SEPARATOR);
+					int i = fileName.lastIndexOf(MIME_SEPARATOR);
 
-					if(i>0) extension = file.substring(i+1);
+					if(i>0) extension = fileName.substring(i+1);
+
+					boolean is_p7m = false;
 
 					if(enabled_p7m && extension.equals(MIME_FORMAT)) {
 
-						String fileWithout_p7m = file.substring(0, i);
+						String fileWithout_p7m = fileName.substring(0, i);
 						int j = fileWithout_p7m.lastIndexOf(MIME_SEPARATOR);
 
-						if(j>0) extension = fileWithout_p7m.substring(j+1);
+						if(j>0) {
+							extension = fileWithout_p7m.substring(j+1); // internal extension
+							is_p7m = true;
+						}
 						else extension = "";
 					}
 
+					Checkmime checkmimeCurrent = null;
+					boolean validated = false;
+
 					for(Checkmime checkmime : checkmimeList) {
-						if(!validated) validated = extension.equalsIgnoreCase(checkmime.getFormat());
-						else break;
+						if(!validated) {
+							validated = extension.equalsIgnoreCase(checkmime.getFormat());
+							if(validated) {
+								checkmimeCurrent = checkmime;
+								break;
+							}
+						}
+					}
+
+					if(validated) {
+
+						try {
+							String descriptionMimeFormat = getMimeFormat(filePath.toFile());
+							validated = descriptionMimeFormat.equals(is_p7m ? description_p7m : checkmimeCurrent.getDescription());
+
+							System.out.println(descriptionMimeFormat);
+
+						} catch (IOException e) {
+							validated = false;
+							throw new RuntimeException(e);
+						}
 					}
 
 					ValidationFormat validationFormat = ValidationFormat.builder()
-							.filename(file)
+							.filename(fileName)
 							.validated(validated)
 							.build();
 					validationList.add(validationFormat);
@@ -117,6 +157,23 @@ public class ValidationController {
 		}
 
 		return ResponseEntity.status(HttpStatus.OK).body(layoutResponses);
+	}
+
+	private String getMimeFormat(File f) throws IOException {
+
+		String mimeType = null;
+		try {
+			InputStream is = new FileInputStream(f);
+			TikaConfig tc = new TikaConfig();
+			Metadata md = new Metadata();
+			md.set(Metadata.RESOURCE_NAME_KEY, f.getName());
+			mimeType = tc.getDetector().detect(TikaInputStream.get(is), md).toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return mimeType;
 	}
 
 }
--- checkmime/src/main/resources/import_checkmime_table.sql
+++ checkmime/src/main/resources/import_checkmime_table.sql
@@ -4,22 +4,22 @@
 -- @2023
 ------------------------------
 INSERT INTO checkmimes (id, format, description, enabled)
-SELECT * FROM (SELECT 1, 'PDF', "PDF file", true) AS tmp
+SELECT * FROM (SELECT 1, 'PDF', 'application/pdf', true) AS tmp
 WHERE NOT EXISTS (
     SELECT format FROM checkmimes WHERE format = 'PDF'
 ) LIMIT 1;
 INSERT INTO checkmimes (id, format, description, enabled)
-SELECT * FROM (SELECT 2, 'TXT', "Text file", false) AS tmp
+SELECT * FROM (SELECT 2, 'TXT', 'text/plain', false) AS tmp
 WHERE NOT EXISTS (
     SELECT format FROM checkmimes WHERE format = 'TXT'
 ) LIMIT 1;
 INSERT INTO checkmimes (id, format, description, enabled)
-SELECT * FROM (SELECT 3, 'java', "Java file", false) AS tmp
+SELECT * FROM (SELECT 3, 'java', 'text/plain', false) AS tmp
 WHERE NOT EXISTS (
     SELECT format FROM checkmimes WHERE format = 'java'
 ) LIMIT 1;
 INSERT INTO checkmimes (id, format, description, enabled)
-SELECT * FROM (SELECT 4, 'p7m', "digitally signed file", true) AS tmp
+SELECT * FROM (SELECT 4, 'p7m', 'application/pkcs7-signature', true) AS tmp
 WHERE NOT EXISTS (
     SELECT format FROM checkmimes WHERE format = 'p7m'
 ) LIMIT 1;
